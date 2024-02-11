1966873d990f4ef7df782c490327673aab5225a2
Argha Mallick <arghamallick5@gmail.com>
2024-02-11 19:54:07 +0530

    Day 11

---
+++ Day 11/index.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const authenticationMiddleware = require('./AuthMiddelware');
+const app = express();
+
+app.use(authenticationMiddleware);
+
+app.get('/protected', (req, res) => {
+    const user = req.user;
+    res.json({ message: `Protected route accessed by user: ${user.username}` });
+});
+
+const port = 3000;
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+});
