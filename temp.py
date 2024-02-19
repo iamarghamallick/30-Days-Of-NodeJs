d641f0825ddedd2646d007f4156993598545ece6
Argha Mallick <arghamallick5@gmail.com>
2024-02-19 14:06:35 +0530

    30 days file setup

---
+++ temp.py
@@ -0,0 +1,10 @@
+# for i in range(1, 31):
+#     with open(f"./Day {i}/README.md", "w") as file:
+#         file.write(f"## Day {i} Problem")
+# with open(f"./Day {i}/index.js", "w") as file:
+#     pass
+
+# import os
+
+# for i in range(21, 31):
+#     os.mkdir(os.path.join("D:/HelloArgha/NODE JS/30-Days-Of-NodeJs/", f"Day {i}"))
